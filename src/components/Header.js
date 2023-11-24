e8842b4455f3035778e1372c1ccc343ed0d69ae9
Рафаэль Гантус <rafael.gantus@bk.ru>
2023-11-24 20:20:15 +0300

    fix

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,28 @@
-import { Link, Routes, Route } from "react-router-dom";
+import React from 'react';
+import { Routes, Route, Link, useNavigate } from "react-router-dom";
 
 export default function Header(props) {
+
+    const navigate = useNavigate();
+
+    function signOut() {
+        localStorage.removeItem('jwt');
+        navigate("/sign-in");
+        window.location.reload()
+    }
+
     return (
         <header className="header">
             <div className="header__logo" />
-            {props.loggedIn ? <div className="header__container">
-                <p className="header__email">{props.userEmail}</p>
-                <button className="header__exit"><Link to="/sign-in" className="register__link">Выйти</Link></button>
-            </div> : ''}
-            <Routes>
-                <Route path='/sign-up' element={<Link to='/sign-in' className="header__sign-up"></Link>} />
-                <Route path='/sign-in' element={<Link to='/sign-up' className="header__login" />} />
-            </Routes>
+             <div className="header__container">
+             {props.loggedIn && <p className="header__email">{props.userEmail}</p>}
+                <Routes>
+                    <Route path='/' element={<Link to='/sign-in' onClick={signOut} className="register__link header__exit">Выйти</Link>} ></Route>
+                    <Route path='/sign-up' element={<Link to='/sign-in' className="header__sign-up"></Link>} />
+                    <Route path='/sign-in' element={<Link to='/sign-up' className="header__login" />} />
+                </Routes>
+            </div>
+
         </header>
     )
 }
